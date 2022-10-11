ac98378d7dc0e9c193ef21d71f85edaa2e7bf060
unknown <kugathasanahilan@gmail.com>
2022-10-11 08:14:33 +0530

    addform

--- head.php
+++ head.php
@@ -1,10 +1,11 @@
+<?php include 'config.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Herbal Lanla Shop</title>
+    <title>Herbal Lanka Shop</title>
     <link rel="icon" href="assets/images/Logo1.png">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/bootstrap-icons.css">
